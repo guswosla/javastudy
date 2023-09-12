2886083f62f19ecc0afefba73fb1b0848fa564aa
sist <sist@DESKTOP-F1TREQD>
2023-09-13 08:12:37 +0900

    1111

---
+++ Chapter2_데이터링/src/변수초기화.java
@@ -0,0 +1,49 @@
+/*/
+ * 
+ * 변수에 초기화 
+ * 1) 변수 추출
+ * 2) 변수 선언 (데이터형)
+ * 3) 변수 초기화
+ * = 명시적인 초기화 
+ * int a=10;
+ * =입력값 
+ * Scanner : 키보드 입력값을 받을 때 사용
+ * BuffereReader:   예외처리
+ * =임의값을 받아서 초기화
+ * ---------랜덤
+ * =크롤링 
+ * 
+ * 
+ * 
+ */
+import java. util. Scanner; //자바에 없을 때 가져올 때 import
+
+public class 변수초기화 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ /*int a=100;
+ int b=(int)( Math.random()*100); 
+//더블(실수)를 정수로 바꿔줘야함, 랜덤으로 값을 출력.
+Scanner scan=new Scanner (System.in);
+//
+System.out.println("정수입력:");
+int c= scan.nextInt();
+System.out.println(c);*/
+		
+		int a=10;
+		int b=0;
+		//a=20, b=10
+		int temp=a; //temp는 빈박스(빈변수) 변수끼리 값교환시 미리 temp에 저장해서 바꿔줌
+		a=b; // a=20
+		a=b; // b=20
+		
+		
+		
+ 
+ 
+
+
+	}
+
+}
---
+++ Chapter2_데이터링/src/자바변수.java
@@ -0,0 +1,59 @@
+/*/
+ * 26page
+ * 형식) 
+ * 데이터형 변수형
+ * -----------
+   fileName, file_name
+   
+   정수값을 저장한다(200)
+   int num=200
+   --- --- ---| 리터럴
+    |    | 변수
+    데이터형
+    =>프로그램 개발 :가독성, 퍼포먼트 (속도)
+                    ----리펙토링(SI/SM)    
+    long l=100
+    char a=65               1.000000
+                       1    1.000001      
+    byte <char< int< long < float <double
+          short 
+          =>메모리 크기(x) => 수표현 
+    각 데이터형 표현법
+   byte =100;
+    int i=100;
+   long l; long l=00==>200L
+   char
+   char c='A', char c=65 ('A')
+   boolean 
+   boolean b=true; b =false;
+   float
+   float f=10.5(f가 없으면 double 값이 커서 저장 안됨)
+   
+   double
+    double d='A'   d=65.0
+    double d=10    d=10.0
+    double d=10.5F d=10.5
+    double d=10L   d=10.5
+        
+          
+   
+   
+ */
+public class 자바변수 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   byte _byte=(byte)300;
+   System.out.println("_byte:"+_byte);
+   //00101100
+   System.out.println(0b00101100); // byte 공간은 8자리, 그 이상의 숫자는 앞자리부터 짤림
+ 
+    
+    
+  
+	}
+
+}
+
+
+ 
