84e91d63117f7d02cc20e0d68a011df569161cb5
sist <sist@DESKTOP-F1TREQD>
2023-09-20 17:52:04 +0900

    1111

---
+++ Chapter03_제어문_(반복문while)2/src/제어문_반복문_1.java
@@ -0,0 +1,75 @@
+/*/
+ * 
+ * while=>1차
+ * -----------응용 (파일 읽기, 데이터베이스(오라클연동))
+ * 일반 프로그램의 반복문(for)
+ * 형식)
+ * 
+ *    초기값=====1
+ *    while(조건식) 2=>무한루프(while(true))
+ *    {
+ *      반복수행 문장3
+ *      증가식4===========>2번으로 이동
+ *                      ----false 종료
+ *                      ----true 문장수행  
+ *    
+ *    }
+ *    ==>응용
+ *    for()
+ *    {  
+ *      for()
+ *       {
+ *       }
+ *    }
+ *    
+ *    --------------------
+ *     
+ *    for()
+ *    {
+ *       while()
+ *       {
+ *          if()
+ *          {
+ *            for()
+ *            {
+ *            } 
+ *          }
+ *       }   
+ *    } 
+ *    -------------------
+ *     
+ *    
+ *    
+ *    
+ * 
+ */
+//1~100까지의 짝수의 합, 홀수의 합
+
+
+public class 제어문_반복문_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int even=0, odd=0;
+    int i=1;
+    while(i<=100)
+    {
+       if(i%2==0)
+    	   even+=i;
+       else
+            odd+=i;
+    
+       i++;
+    	
+    }
+    System.out.println("짝수:"+even);
+    
+    System.out.println("홀수:"+odd);
+  
+    }
+		
+		
+		
+	}
+
+
