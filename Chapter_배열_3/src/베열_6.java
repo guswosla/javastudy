0e24f08aae3f0cad2fb69a3cc2bafc5c80bd6520
sist <sist@DESKTOP-F1TREQD>
2023-09-26 16:59:06 +0900

    1111

--- Chapter_배열_3/src/베열_6.java
+++ Chapter_배열_3/src/베열_6.java
@@ -49,32 +49,30 @@ public class 베열_6 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
-char[] alpha=new char[10];
-for(int i=0; i<alpha.length; i++)
-{
-	alpha[i]=(char)(Math.random()*26+65);
-	
-}
-System.out.println("=====정렬전======");	
-System.out.println(Arrays.toString(alpha));
-System.out.println("=====정렬후======");	
-for(int i=0; i<alpha.length-1; i++)
-{
-	
-	for(int j=i+1; j<alpha.length; j++)
-	{
-		
-		if(alpha[i]>alpha[j]);
-		alpha[i]=alpha[j];
-		alpha[j]=temp;
-		
-		
-	}
-	
-	System.out.println((i+1)+"Round==>"+alpha[i]);
-}
-System.out.println(Arrays.toString(alpha));
-	}
+		  char[] alpha=new char[10];
+	        for(int i=0;i<alpha.length;i++)
+	        {
+	        	alpha[i]=(char)((Math.random()*26)+65);
+	        }
+	        System.out.println("==== 정렬전 ====");
+	        System.out.println(Arrays.toString(alpha));
 
-}
+	        System.out.println("==== 정렬후 ====");
+	        for(int i=0;i<alpha.length-1;i++)
+	        {
+	        	for(int j=i+1;j<alpha.length;j++)
+	        	{
+	        		if(alpha[i]>alpha[j])
+	        		{
+	        			char temp=alpha[i];
+	        			alpha[i]=alpha[j];
+	        			alpha[j]=temp;
+	        		}
+	        	}
+	        	System.out.println((i+1)+"Round==>"+alpha[i]);
+	        	System.out.println(Arrays.toString(alpha));
+	        }
+	        System.out.println(Arrays.toString(alpha));
+		}
+
+	}
