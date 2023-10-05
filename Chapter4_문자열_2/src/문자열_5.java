d0b959992db8c1086b14ff61fafc818f4ee57ba8
sist <sist@DESKTOP-F1TREQD>
2023-10-05 17:19:30 +0900

    1111

---
+++ Chapter4_문자열_2/src/문자열_5.java
@@ -0,0 +1,100 @@
+import java.io.*;
+import java. util.*; 
+/*/
+ * 
+ * 라이브러리 
+ * -------
+ * 
+ * java.lang: Object, String, StringBuffer, Math
+ *         Wrapper
+ * java.util : List , Set , Map
+ * java.io:
+ * java.net
+ * java.text
+ * ---------------------------------------------------
+ * 
+ * 
+ */
+
+public class 문자열_5 {
+static String movie; 
+static
+{
+	try
+	{
+		FileReader fr=new FileReader("c:\\javaDev\\movie.txt");
+		int i=0;
+		StringBuffer data=new StringBuffer();
+		while( (i=fr.read())!=-1 )
+			
+		{
+			data.append((char)i);
+		
+		}
+		
+		movie=data.toString();
+		
+		
+	}catch( Exception ex) {}
+	
+	
+	
+}
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+        //파일 읽기
+		//System.out.println(movie);
+		String[] movieData=movie.split("\n");
+		//System.out.println("영화 총갯수"+ movieData.length);
+		//System.out.println(movieData[0]);
+		System.out.println("===============영화 목록================");
+		//검색
+		//송강호가 출연한 영화를 출력하시오
+		Scanner scan=new Scanner(System.in);
+		//System.out.println("이름 입력:" );
+		// String name=scan.next();
+		 //1990에 개봉한 영화
+		 //12세 이상 영화 , startsWith, contains 둘다 됨 
+		System.out.println("등급 입력:" );
+		int grade=scan.nextInt();
+		
+	   for(String movie:movieData)
+	         {
+	    	  
+	    	 String[] detail=movie.split("\\|");
+	    	 if(detail[6].startsWith(String.valueOf(grade)) )//입력한 것 찾아줌 
+	    			 {
+	    	 System.out.println("순위:"+detail[0] );
+	    	 System.out.println("영화명:"+detail[1] );
+	    	 System.out.println("장르:"+detail[2] ); 
+	    	 System.out.println("출연:"+detail[3] ); 
+	    	 System.out.println("개봉일:"+detail[4] ); 
+	    	 System.out.println("등급 :"+detail[5] ); 
+	    	 System.out.println("감독 :"+detail[6] ); 
+	    	 System.out.println("----------------------------------------"); 
+	    	 
+	    			 }
+	         }
+	
+	/*   for(String movie:movieData)
+       {
+  	  
+  	 String[] detail=movie.split("\\|");
+  	 if(detail[4].contains("1990"))//
+  			 {
+  	 System.out.println("순위:"+detail[0] );
+  	 System.out.println("영화명:"+detail[1] );
+  	 System.out.println("장르:"+detail[2] ); 
+  	 System.out.println("출연:"+detail[3] ); 
+  	 System.out.println("개봉일:"+detail[4] ); 
+  	 System.out.println("등급 :"+detail[5] ); 
+  	 System.out.println("감독 :"+detail[6] ); 
+  	 System.out.println("----------------------------------------"); 
+  	 
+  			 }
+       }  */
+	     
+		
+	}
+
+}
